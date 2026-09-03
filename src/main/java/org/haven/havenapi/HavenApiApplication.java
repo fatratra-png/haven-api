2e10f2dfe2383c9a743c5047a18e10d47d441849
fatratra-png <nyfatratrar@gmail.com>
2026-09-03 09:07:54 +0300

    Initial commit

---
+++ src/main/java/org/haven/havenapi/HavenApiApplication.java
@@ -0,0 +1,13 @@
+package org.haven.havenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HavenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HavenApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/haven/havenapi/HavenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.haven.havenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HavenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
