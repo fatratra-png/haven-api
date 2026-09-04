1aec73e2a1621bb2bdd2618ec81e5ca4335a3e5a
fatratra-png <nyfatratrar@gmail.com>
2026-09-04 13:33:41 +0300

    feat: create user creation repository

---
+++ src/main/java/org/haven/havenapi/repository/UserRepository.java
@@ -0,0 +1,46 @@
+package org.haven.havenapi.repository;
+
+import org.haven.havenapi.config.DatabaseConnection;
+import org.haven.havenapi.dto.CreateUserDTO;
+import org.haven.havenapi.exception.DatabaseException;
+import org.haven.havenapi.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
+
+public class UserRepository {
+    private User mapRow(ResultSet rs) throws SQLException {
+        return new User(
+                rs.getString("id"),
+                rs.getString("user_name"),
+                rs.getInt("age"),
+                User.Gender.valueOf(rs.getString("gender"))
+        );
+    }
+
+    public User create(CreateUserDTO createRequest) {
+        String createQuery = "INSERT INTO users(user_name,age,gender) VALUES(?,?,?) RETURNING id,user_name,age,gender";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(createQuery);
+        ) {
+            ps.setString(1, createRequest.userName());
+            ps.setInt(2, createRequest.age());
+            ps.setString(3, createRequest.gender().name());
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return mapRow(rs);
+                }
+                throw new DatabaseException("Creationg user: no row returned");
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Creation failed", e);
+        }
+    }
+
+}
