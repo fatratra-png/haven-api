2a098d4cf7f7618d1442e99870d1f9ce2699df70
fatratra-png <nyfatratrar@gmail.com>
2026-09-04 20:40:12 +0300

    feat: create not found user exception handler

---
+++ src/main/java/org/haven/havenapi/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package org.haven.havenapi.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String id) {
+        super("Could not find user with id: " + id);
+    }
+}
