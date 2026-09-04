d6309b5bb6ef736a8c27d52b324b193affbb3073
fatratra-png <nyfatratrar@gmail.com>
2026-09-04 19:55:36 +0300

    feat: add POST /users endpoint in UserController

--- src/main/java/org/haven/havenapi/controller/UserController.java
+++ src/main/java/org/haven/havenapi/controller/UserController.java
@@ -1,21 +1,29 @@
 package org.haven.havenapi.controller;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.haven.havenapi.dto.CreateUserDTO;
 import org.haven.havenapi.model.User;
 import org.haven.havenapi.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@AllArgsConstructor
-
 @RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+
 public class UserController {
+
     private final UserService userService;
 
-    @GetMapping("/users")
-    public List<User> getUsers() {
-        return userService.getAllUsers();
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody CreateUserDTO createUserDTO) {
+        User created = userService.createUser(createUserDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
+
+
 }
