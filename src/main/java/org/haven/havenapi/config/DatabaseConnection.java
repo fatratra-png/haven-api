1d810fbd5251580529c0663e997ed856859b736f
fatratra-png <nyfatratrar@gmail.com>
2026-09-04 13:24:49 +0300

    fix(conn): add Connection as getConnection return type and put the method as static

--- src/main/java/org/haven/havenapi/config/DatabaseConnection.java
+++ src/main/java/org/haven/havenapi/config/DatabaseConnection.java
@@ -1,19 +1,18 @@
 package org.haven.havenapi.config;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.stereotype.Component;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
-import org.springframework.stereotype.Component;
-
-import io.github.cdimascio.dotenv.Dotenv;
 
 @Component
 public class DatabaseConnection {
 
     private final Dotenv dotenv = Dotenv.load();
 
-    public Connection getConnection() throws SQLException {
+    public static Connection getConnection() throws SQLException {
         String url = dotenv.get("DB_URL");
         String username = dotenv.get("DB_USERNAME");
         String password = dotenv.get("DB_PASSWORD");
