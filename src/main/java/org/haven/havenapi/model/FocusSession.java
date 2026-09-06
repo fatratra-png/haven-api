ceca08912bd07afe64a17858b28af8c1448d1d10
fatratra-png <nyfatratrar@gmail.com>
2026-09-06 10:51:23 +0300

    fix: add userId attribute in journal entry model to sync with the db table

--- src/main/java/org/haven/havenapi/model/FocusSession.java
+++ src/main/java/org/haven/havenapi/model/FocusSession.java
@@ -3,5 +3,5 @@ package org.haven.havenapi.model;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-public record FocusSession(String id, Duration duration, LocalDateTime startedAt, boolean completed) {
+public record FocusSession(String id, String userId, Duration duration, LocalDateTime startedAt, boolean completed) {
 }
